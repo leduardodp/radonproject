6860f8281bbf4947012741ce2a8ec6d47b86979e
leduardodp <156515398+leduardodp@users.noreply.github.com>
2024-03-05 20:52:00 +0100

    Update views.py

--- users/views.py
+++ users/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
